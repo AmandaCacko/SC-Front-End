296328919ebe1833cb21b141b43b308d3e53ac66
amandacacko <amandacacko@gmail.com>
2023-11-20 12:38:43 -0300

    Aula 2 - Operadores e Variáveis

--- primeiro.js
+++ primeiro.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+/* Aula 1 - Conhecendo JS
+Comentário duas barras -> // */
+// Estou imprimindo duas mensagens no terminal
+
+console.log("Hello, world!")
+console.log("Bem-vindo ao curso de JavaScript!")
---
+++ variaveis.js
@@ -0,0 +1,27 @@
+// Aula 2 - Variáveis 
+
+let idade = 26
+console.log(idade)
+
+// Tipos ve variáveis
+
+idade = 20;             // Number
+let numero = 30;        // Number
+let altura = 1.65       // Number
+let nome = "Amanda"     // String
+let estudando = true    // Boolean
+
+console.log(nome, typeof nome)
+console.log(idade, typeof idade)
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
+
+let outra
+console.log(outra)
+
+let nula = null
+console.log(nula)
+
+var variavel = 30 // Antiga
+const constante = 100 // Não alterável
