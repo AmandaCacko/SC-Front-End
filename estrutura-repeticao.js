70241f7d002bf1857cc8493b017a2f2ad4c88ba5
amandacacko <amandacacko@gmail.com>
2023-11-22 15:17:32 -0300

    Estruturas de Repetição e Laços Condicionais

---
+++ estrutura-repeticao.js
@@ -0,0 +1,14 @@
+// Aula 6 - Estruturas de Repetição e Laços Condicionais
+
+const input = require("readline-sync")
+
+const numero_sorteado = 3;
+
+let numero = Number(input.question("Qual numero voce escolhe? "))
+
+while (numero !== numero_sorteado) {
+    console.log("Você errou o número. Tente novamente...")
+    numero = Number(input.question("Qual numero voce escolhe? "))
+}
+
+console.log("Você acertou!!!")
